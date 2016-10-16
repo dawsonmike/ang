32e38885f74faac6cbcd3ef6bffb441eead3af59
dawsonmike <dawsonmike@gmail.com>
2016-10-17 00:00:56 +0100

    Adding split controller declared needs array

--- app/controllers/view2ctl.js
+++ app/controllers/view2ctl.js
@@ -1,15 +1,6 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute'])
-
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
-    templateUrl: 'views/view2.html',
-    controller: 'View2Ctrl'
-  });
-}])
-
-.controller('View2Ctrl', function($scope) {
+angular.module('myApp.view2').controller('View2Ctrl', function($scope) {
 $scope.h=4;
 $scope.m="mike";
 });
